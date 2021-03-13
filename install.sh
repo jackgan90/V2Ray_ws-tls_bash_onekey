685097402d60b002fe80a585cf359a396bae4078
jackgan90 <ganjiework@163.com>
2021-03-13 13:36:54 +0800

    change the random generated camflague path to the fixed one

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ v2ray_conf="${v2ray_conf_dir}/config.json"
 nginx_conf="${nginx_conf_dir}/v2ray.conf"
 
 #Generate a random path to be used as websocket path
-camouflage=`cat /dev/urandom | head -n 10 | md5sum | head -c 8`
+camouflage="bridge"
 
 source /etc/os-release
 
